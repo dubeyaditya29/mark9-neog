92fd709857dd07f0705e02b1844f95e878b78269
aditya dubey <48158220+dubeyaditya29@users.noreply.github.com>
2021-09-23 03:43:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import Header from "./components/Header";
+import { useState } from "react";
+export default function App() {
+  const movies = ["kagaz", "kalam", "Jab We Met", "raazi"];
+  const foods = ["kachori", "samosa", "jalebi", "rasogulla"];
+  const books = [
+    "Meluha",
+    "The book of ram",
+    "The subtle art of not giving a f*ck",
+    "Eat That frog"
+  ];
+  const [list, setList] = useState([]);
+  const clickHandler = (value) => {
+    setList(value);
+  };
+  return (
+    <div className="App">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="btn-div">
+        <button onClick={() => clickHandler(movies)}>Movies</button>
+        <button onClick={() => clickHandler(foods)}>Foods</button>
+        <button onClick={() => clickHandler(books)}>Books</button>
+      </div>
+      <div className="output-div">
+        {list.map((ele, index) => (
+          <p key={index}>{ele}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h2>Checkout the below Recomendations</h2>;
+};
+
+export default Header;
